eb7a4189d0e9357632900a96ee4ffc93d65e7797
Hasna Maulida <161681551+hsnaamld@users.noreply.github.com>
2024-03-08 14:55:16 +0700

    Fix bug in user id retrieval process and update UI
    The previous implementation for fetching user id in the book borrowing form was flawed, resulting in incorrect user id assignments. This commit addresses the issue by properly retrieving the user id from the database, ensuring accurate user identification during book borrowing process. Additionally, UI improvements have been made to enhance the overall user experience, including adjustments to layout, styling, and usability.

--- peminjam/data/pinjam_buku.php
+++ peminjam/data/pinjam_buku.php
@@ -9,24 +9,30 @@
 </head>
 
 <body>
-    <div class="container">
+    <div class="container" style="width:50rem">
         <div class="content">
-            <div class="card rounded border-dark shadow" style="margin-top:8rem">
+            <div class="card rounded border-dark shadow mb-3 mt-4" >
                 <div class="row">
                     <div class="col mb-4" style="margin-left:4rem; margin-right:4rem">
                         <h1 class="text-center mt-3">Form Peminjaman</h1>
-                        <a href="../buku.php">Kembali</a>
+                        <a href="../buku.php" class="text-center">Kembali</a>
                         <?php
                         session_start();
-                        $iduser = $_SESSION['id_user'];
                         include '../../koneksi.php';
+                        $iduser = $_SESSION['id_user'];
+                        $idbuku1=$_GET['idbuku'];
+                        $cekstatus=mysqli_query($koneksi,"SELECT * from peminjaman where IDuser='$iduser'and IDbuku='$idbuku1'and status_peminjaman='dipinjam'");
+                        if(mysqli_num_rows($cekstatus)>0){
+                            header("location:../buku.php?status=dipinjam");
+                        }
+
                         if (isset($_GET['idbuku'])) {
                             $idbuku = $_GET['idbuku'];
                         } else {
                             die("error, data tidak ditemukan");
                         }
                         $data = mysqli_query($koneksi, "SELECT * from buku where IDbuku=$idbuku");
-                        $d = mysqli_fetch_array($data);
+                        $d = mysqli_fetch_assoc($data);
                         ?>
                         <form method="post" action="t_pinjam.php">
                             <div class="form-group mt-2">
@@ -34,8 +40,8 @@
                                 <input type="text" id="username" name="idbuku" value="<?php echo $d['IDbuku']; ?>" readonly class="form-control" required>
                             </div>
                             <div class="form-group mt-2">
-                                <label for="username" class="form-label">IDuser</label>
-                                <input type="text" id="username" name="iduser" placeholder="input IDuser" class="form-control" required>
+                                <label for="username" class="form-label">ID user</label>
+                                <input type="text" id="username" name="iduser" class="form-control" value="<?php echo $iduser ?>" readonly  required>
                             </div>
                             <div class="form-group mt-2">
                                 <label for="username" class="form-label">Nama</label>
@@ -53,7 +59,7 @@
                                 </select>
                             </div>
                             <div class="form-group text-center">
-                                <button type="submit" class="btn btn-danger" style="margin-top:3rem;">Pinjam</button>
+                                <button type="submit" class="btn btn-danger mt-3" >Pinjam</button>
                             </div>
                         </form>
                     </div>
