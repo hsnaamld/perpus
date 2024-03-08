d180b89fbc7c8d0c4d70b79677241c5fe4952272
Hasna Maulida <161681551+hsnaamld@users.noreply.github.com>
2024-03-08 20:10:01 +0700

    style: add background color and update register page layout

--- register.php
+++ register.php
@@ -8,13 +8,13 @@
     <link rel="stylesheet" href="asset/bootsrap/css/bootstrap.min.css">
 </head>
 
-<body>
+<body style="background-color:#F8EDFF;">
     <div class="container">
         <div class="content">
-            <div class="card rounded border-danger shadow" style="margin-top:8rem">
+            <div class="card rounded shadow" style="margin-top:3rem; margin-bottom:3rem" >
                 <div class="row">
                     <div class="col" style="margin-left:5rem; margin-top:5rem">
-                        <img src="asset/img/smk5.png" alt="smk 5">
+                        <img src="asset/img/smk5.png" width="400" style="margin-left:1rem;" alt="smk 5">
                     </div>
                     <div class="col mb-4" style="margin-left:4rem; margin-right:4rem">
                         <h1 class="text-center mt-3">Register</h1>
@@ -48,9 +48,9 @@
                                 </select>
                             </div>
                             <div class="form-group text-center">
-                                <button type="submit" class="btn btn-danger" style="margin-top:3rem;">Register</button>
+                                <button type="submit" class="btn btn-danger" style="margin-top:2rem;">Register</button>
                             </div>
-                            <p class="mt-3">Sudah punya akun? <a href="index.php" class="btn btn-outline-danger">Login</a></p>
+                            <p class="mt-3 text-center">Sudah punya akun? <a href="index.php" class="btn btn-outline-danger">Login</a></p>
                         </form>
                     </div>
                 </div>
