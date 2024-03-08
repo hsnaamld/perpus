23567eed33621ad034bfec363910595e0c07a7d7
Hasna Maulida <161681551+hsnaamld@users.noreply.github.com>
2024-03-08 15:18:19 +0700

    feat: add alert notification

--- peminjam/bukusaya.php
+++ peminjam/bukusaya.php
@@ -3,15 +3,15 @@ session_start();
 include 'layout/header.php';
 ?>
 <div class="container">
+    <div class="content">
     <div class="row">
         <div class="col">
-            <div class="card border-dark" style="margin-top:3rem; height:5rem; text-align:center;">
-                <h2 class="mt-3">Daftar Buku</h2>
+            <div class="card border-dark" style="margin-top:2rem; text-align:center;">
+                <h2 class="fw-medium">Buku Saya</h2>
             </div>
         </div>
     </div>
-</div>
-<div class="container">
+
     <div class="row">
         <?php
         include '../koneksi.php';
@@ -26,9 +26,10 @@ include 'layout/header.php';
                 $data = mysqli_query($koneksi, "SELECT * from buku where IDbuku='$idbuku'");
                 while ($d = mysqli_fetch_array($data)) {
         ?>
-                    <div class="card mt-4" style="width:16rem; height:20rem; margin-right: 2rem">
-                        <img src="<?php echo "../petugas/asset/sampul/$d[foto]"; ?>" class="m-2" alt="" width="200">
-                        <div class="card-title m-2">
+                    <div class="card mt-4" style="width:14rem; margin-left: 3px">
+                        <img src="<?php echo "../petugas/asset/sampul/$d[foto]" ?>" class="card-img-top" style="margin-top:1rem; margin-right:1rem;" alt="">
+                          <div class="card-body">
+                        <div class="card-title">
                             <h5><?php echo $d['judul']; ?></h5>
                             <h6><?php echo $d['tahunterbit']; ?></h6>
                             <a href="data/detail.php?idbuku=<?php echo $d['IDbuku']; ?>" class="btn btn-outline-danger mt-3" style="width:85px">Baca</a>
@@ -37,15 +38,16 @@ include 'layout/header.php';
                     </div>
                 <?php
                 }
-                ?>
-            <?php
             }
-            ?>
-        <?php
         } else {
-            echo "belum ada buku yang dipinjam";
+            $alert = '<div class="mt-4 alert alert-warning alert-dismissible fade show" role="alert">
+              <strong>Belum ada buku yang dipinjam.</strong> Silahkan pinjam buku
+            </div>';
+            echo $alert;
+            
         }
         ?>
+    </div>
     </div>
 </div>
 <?php
