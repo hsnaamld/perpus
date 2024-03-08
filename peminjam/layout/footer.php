3cfcd300dd0d2c292668fd775d25987d91150bae
Hasna Maulida <161681551+hsnaamld@users.noreply.github.com>
2024-03-08 15:21:29 +0700

    fix javascript

--- peminjam/layout/footer.php
+++ peminjam/layout/footer.php
@@ -1,5 +1,5 @@
-<script src="../asset/bootsrap/js/bootstrap.min.js">
-    < /body>
+<script src="../asset/bootsrap/js/bootstrap.min.js"> </script>
+    
+    </body>
 
-    <
-    /html>
+    </html>
